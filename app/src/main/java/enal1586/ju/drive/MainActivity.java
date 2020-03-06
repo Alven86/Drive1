83759ad5021006f64ff57af02e55549cefdc03af
Alven <alvenj@yahoo.com>
2020-03-06 09:05:12 +0100

    Connect to firebase and create welcome activity.

--- app/src/main/java/enal1586/ju/drive/MainActivity.java
+++ app/src/main/java/enal1586/ju/drive/MainActivity.java
@@ -2,6 +2,7 @@ package enal1586.ju.drive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    Thread thread = new Thread()
+    {
+        @Override
+        public void run()
+        {
+            //start the main screen for som secondes then continue to welcome screen.
+            try
+            {
+                sleep(8000);
+            }
+
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            finally
+            {
+                Intent mainIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+                startActivity(mainIntent);
+            }
+        }
+    };
+        thread.start();
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+
+        finish();
     }
 }
